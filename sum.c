0697c06664e4cf9dffdd087650ec11cd99b1a737
kanishknene-ui <kanishk.nene@gmail.com>
2025-08-23 00:08:43 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int x=45;
+    int y=30;
+    int z=0;
+    int w=0;
+    int v=0;
+    int t=0;
+
+    "sum";
+    z=x+y;
+    printf("the sum of the above two number is %d\n",z);
+    "subtract";
+    w=x-y;
+    printf("the difference of the two number is %d\n",w);
+    "multiplication";
+    v=x*y;
+    printf("the product of the two number is %d\n",v);
+    "division";
+    t=x/y;
+    printf("the quotient of the two number is %d\n",t);
+    return 0;
+}
---
+++ sum1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int x=12;
+    int y=14;
+    int z=0;
+    z=x+y;
+    printf("the sum of the two number is %d\n",z);
+    return 0;
+}
