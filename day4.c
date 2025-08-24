62f401f744d9dc4a0b0ca06bae86df455f23eacd
kanishknene-ui <kanishk.nene@gmail.com>
2025-08-24 18:18:41 +0530

    Add files via upload

---
+++ day4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a=17;
+    int b=14;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("the value of a is %d\n",a);
+    printf("the value of b is %d\n",b);
+    
+    return 0;
+
+}
